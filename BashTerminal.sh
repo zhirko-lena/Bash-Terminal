e30c1727fc55b59b40157cf3ba5561f954cec6a5
root <root@localhost.localdomain>
2019-04-08 14:21:35 +0300

    to DEV

--- BashTerminal.sh
+++ BashTerminal.sh
@@ -1,1 +1,19 @@
 #! /usr/bin/env bash
+
+clear
+Menu(){
+  echo -e "1. Packet manager\n0. Exit";
+  read choice
+
+}
+
+exit=true
+while [ $exit == true ]
+do
+  Menu;
+  case $choice in
+    1) echo "Packet manager"; ;;
+    0) echo "Bye!"; let exit=false; ;;
+    *) echo "Wrong choice!"; ;;
+  esac
+done
